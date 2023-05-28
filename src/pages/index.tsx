4f83d60b895cb3ca14a2a6b0a9542016fd4c9963
David Papp <pappdavidlevele@gmail.com>
2023-05-28 19:51:34 +0200

    cesiumok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,8 +15,34 @@ export default function Home() {
     const viewer = new Cesium.Viewer("cesiumContainer", {
       terrainProvider: Cesium.createWorldTerrain(),
     });
+    var cityCoordinates = Cesium.Cartesian3.fromDegrees(
+      19.040235,
+      47.497912,
+      15000.0
+    );
+    viewer.scene.camera.setView({
+      destination: cityCoordinates,
+      orientation: {
+        heading: Cesium.Math.toRadians(0),
+        pitch: Cesium.Math.toRadians(-90),
+        roll: Cesium.Math.toRadians(0),
+      },
+    });
+
+    viewer.entities.add({
+      // long, lat, height
+      position: Cesium.Cartesian3.fromDegrees(19.0551, 47.49865, 0),
+      point: {
+        pixelSize: 20,
+        color: Cesium.Color.fromCssColorString("#2a6da3"),
+        outlineColor: Cesium.Color.fromCssColorString("#a87136"),
+        outlineWidth: 5,
+      },
+    });
     Cesium.Ion.defaultAccessToken =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmNThhZjBkMC1iZTdmLTQ0MDYtYmQwMi00OTYxNzNkZDM3NDIiLCJpZCI6MTQxOTY4LCJpYXQiOjE2ODUyMDg1NDR9.ZIPgYSxABC6BOVo095Np_vpmB2yGInjCb4O0BNSqR30";
   }, []);
-  return <div id="cesiumContainer" style={{ height: "50vh", width: "50vw" }} />;
+  return (
+    <div id="cesiumContainer" style={{ height: "100vh", width: "100vw" }} />
+  );
 }
