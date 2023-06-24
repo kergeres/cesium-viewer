b5ca4d8eea9eacbc000bfe19899d4aee6d88b04e
Papp Dávid <pappdavidlevele@gmail.com>
2023-06-24 21:31:58 +0200

    layout involve footer

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Login from "./login";
-import Footer from "./footer";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 import Header from "./header";
 import { useSession } from "next-auth/react";
+import { BottomNavigation } from "@mui/material";
 
 const Layout = ({ children = <Login /> }) => {
   const { data: session } = useSession();
@@ -12,7 +13,21 @@ const Layout = ({ children = <Login /> }) => {
     <>
       <Header />
       {children}
-      {session && <Footer />}
+      {session && (
+        <BottomNavigation
+          sx={{
+            backgroundColor: "#3c4043",
+            bottom: 0,
+            position: "absolute",
+            width: "100vw",
+          }}
+          showLabels
+        >
+          <a target="_blank" href="https://github.com/kergeres">
+            <GitHubIcon sx={{ m: 2 }}></GitHubIcon>
+          </a>
+        </BottomNavigation>
+      )}
     </>
   );
 };
